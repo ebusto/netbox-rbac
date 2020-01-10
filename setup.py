71ffe7304f26a0fcb37469d9fbe5d408742c6f1d
Eric Busto <ebusto@nvidia.com>
2020-01-10 14:21:44 -0800

    Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
 	name='netbox-rbac',
-	version='1.0.0',
+	version='1.0.1',
 	author='Eric Busto',
 	author_email='ebusto@nvidia.com',
 	description='Opinionated RBAC for NetBox',
